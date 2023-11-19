779325f1008423d4bc0faed02d1c68e1e5a2c44b
Yeaji Jung <by1094@naver.com>
2023-11-19 23:29:11 +0900

    Feat: morgan middleware 기능 추가 1. 기존 로그 외 추가적인 내용 확인을 위해 morgan 추가

--- src/app.mjs
+++ src/app.mjs
@@ -1,12 +1,15 @@
 import 'dotenv/config' 
 import express from 'express';
 import morgan from 'morgan';
-import cookieParser from 'cookie-parser';
-import session from 'express=session';
-import dotenv
-
 
 const app = express();
+
+app.use(morgan('dev')); 
+
+app.use((req,res,next)=>{
+    console.log('모든 요청에 다 실행됩니다.');
+    next();
+})
 
 app.get('/',(req,res)=>{
     res.send("hello, node..");
