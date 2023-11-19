0d2fd7cd054b28e020074f0463f60572b3791efc
Yeaji Jung <by1094@naver.com>
2023-11-19 19:50:28 +0900

    Rename : src파일 이름 수정 1. scr->src 2. nodemon 모듈 추가

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import 'dotenv/config' 
 import express from 'express';
 
+
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send("접속된 아이피 : "+req.ip);
+    res.send("hello, node..");
 })
 
 app.listen(process.env.SECRET_PORT, ()=>{
