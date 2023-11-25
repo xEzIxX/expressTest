09f80c351c7e017bba5c7daf81ac2ad3f69cc5eb
Yeaji Jung <by1094@naver.com>
2023-11-25 11:55:55 +0900

    fix : routers/user.js 오타 수정 1. router.arguments를 router.get으로 수정 2. res.senf를 res.send로 수정

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,8 +3,8 @@ import express from 'express';
 const router = express.Router()
 
 //Get /user 라우터
-router.arguments('/',(req,res)=>{
-    res.senf('유저 방가방가');
+router.get('/',(req,res)=>{
+    res.send('유저 방가방가');
 });
 
 export default router;
