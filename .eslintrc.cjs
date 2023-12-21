2e638e309d5b9dc27a8311ade763097b13b96854
xEzIxX <by1094@gachon.ac.kr>
2023-12-21 19:31:45 +0900

    feat:eslint ESM 세팅 1. .eslintrc.js는 ESM Load 관련 문제로 사용할 수 없음    따라서 cjs로 파일명 설정하여 업로드 2. package.json, .lock.json    eslint 설정에 따라 추가로 모듈 설치함

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,35 +1,26 @@
 module.exports = {
-    "root":true,
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: 'standard',
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      }
     }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+  }
 }
