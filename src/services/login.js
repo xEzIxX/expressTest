bc21cfad1a394fc0e0c54d673e146d71e03aab5f
xEzIxX <by1094@gachon.ac.kr>
2024-01-02 01:23:27 +0900

    feat : 로그인 서비스 기능 추가

---
+++ src/services/login.js
@@ -0,0 +1,17 @@
+export const login = function() {
+    //로그인 페이지 제공
+};
+export const checkLog = function() {
+    //입력한 정보가 올바른지 체크
+};
+
+export const logout = function() {
+    //로그아웃
+};
+
+export const signUp = function() {
+    //회원가입 페이지
+};
+export const signUpHandler = function() {
+    //회원정보 저장
+};
