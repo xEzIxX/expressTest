c387fdee00c0ce969cb9db33bbe2c5a882d0cd15
xEzIxX <by1094@gachon.ac.kr>
2024-01-03 14:32:46 +0900

    feat : 파일 이름을 login.js->auth.js으로 변경 1. src/routers/login.js의 js파일 이름을 auth.js으로 변경 2. 이름 변경에 따른  app.js에서 불러온 routers 경로 수정

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import express from 'express';
 import morgan from 'morgan';
 
-import loginRouter from './routers/login.js';
+import loginRouter from './routers/auth.js';
 
 const app = express();
 
