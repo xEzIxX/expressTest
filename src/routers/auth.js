cfd7505622499496baa854d14025919ddf2b6753
xEzIxX <by1094@gachon.ac.kr>
2024-01-07 23:11:12 +0900

    feat : routers/auth.js 수정 1. 라우터 경로 수정 : authRouter.route(path).METHOD(callback) -> authRouter.METHOD(path,callback) 2. try..catch문을 통한 에러핸들링 3. src/services/auth.js에서 Auth 클래스 불러옴 4. src/services/auth.js에서 불러온 클래스의 메소드 사용 : Auth.method, 그 이전에는 메소드 이름으로 불러오려고 하였었음(잘못된 방식)

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,14 +1,45 @@
 import express from 'express'
+import { Auth } from '../services/auth.js'
 const authRouter = express.Router()
 
-authRouter.route('/')
-    .get(login)
-    .post(checkLog)
+authRouter.get('/', function (req, res) {
+    try {
+        Auth.login()
+    } catch (err) {
+        console.log(err)
+    }
+})
 
-authRouter.get('/logout', main)
+authRouter.post('/', function (req, res) {
+    try {
+        Auth.checkLog()
+    } catch (err) {
+        console.log(err)
+    }
+})
 
-authRouter.route('/sign')
-    .get(signUp)
-    .post(signUpHandler)
+authRouter.get('/logout', function (req, res) {
+    try {
+        Auth.main()
+    } catch (err) {
+        console.log(err)
+    }
+})
 
-export {authRouter}
+authRouter.get('/sign', function (req, res) {
+    try {
+        Auth.signUp()
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+authRouter.post('/sign', function (req, res) {
+    try {
+        Auth.signUpHandler()
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+export { authRouter }
