1ca10189e114c9734ca48bad6d6e391e1c3bd7f2
xEzIxX <by1094@gachon.ac.kr>
2024-01-10 19:21:42 +0900

    feat : services/auth.js 수정 1. Auth 클래스 이름 AuthServices로 수정 - 서비스 메소드를 포함하는 클래스임을 더 명확하게 표현 2. checkLog, main 메소드 삭제 -  이미 존재하는 login, signUp 메소드에서 수행할 역할임, 불필요하여 삭제 3. try..catch문을 사용함 - catch문에서 err를 받아 에러 핸들링

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,21 +1,17 @@
-export class Auth {
+export class AuthService {
     login() {
-        res.render('./views/loginPage')
-    }
-
-    checkLog() {
-        // 입력한 정보가 올바른지 체크
-    }
-
-    main() {
-        res.render('./views/main')
+        try {
+            // 사용자의 정보를 입력받아 데이터 베이스에 이미 존재하는 데이터인지 확인
+        } catch (err) {
+            console.log(err)
+        }
     }
 
     signUp() {
-        res.render('./views/signup')
-    }
-
-    signUpHandler() {
-        // 회원정보 저장
+        try {
+            // 회원 가입, 데이터 베이스에 회원 정보 저장
+        } catch (err) {
+            console.log(err)
+        }
     }
 }
