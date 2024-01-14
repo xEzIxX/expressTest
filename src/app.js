1f92d0b617697f403e3277c3067e47200ef5378c
xEzIxX <by1094@gachon.ac.kr>
2024-01-14 19:58:27 +0900

    feat : app.js에 sequelizeLoader 불러오고 사용 1. loader 파일에 정의된 sequelizeLoader 불러오기 2. sequelizeLoader를 통하여 시퀄라이즈 모델 연결 및 동기화

--- src/app.js
+++ src/app.js
@@ -2,12 +2,15 @@ import 'dotenv/config'
 import express from 'express'
 import morgan from 'morgan'
 import { authRouter } from './routers/auth.js'
+import { sequelizeLoader } from './loaders/sequelize.js'
 
 const app = express()
 
 app.use(morgan('dev'))
 
 app.use('/auth', authRouter)
+
+await sequelizeLoader()
 
 app.use((err, req, res, next) => {
     res.status(404).send('Not Found')
