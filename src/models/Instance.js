629dc566d8dcdb78080c1517e6b4fb101904e553
xEzIxX <by1094@gachon.ac.kr>
2024-01-15 16:39:10 +0900

    feat : 시퀄라이즈 인스턴스 생성, 분리 1. models/index.js에서 시퀄라이즈 인스턴스 생성, 데이터 베이스 관리를 하였으나  데이터 베이스와 모델 인스턴스 사이의 연결과  시퀄라이즈 모델을 define하여 모델 인스턴스 생성 시에 경로가 겹치는 현상이 발생하였습니다.  따라서 모델 인스턴스 생성 부분만 분리하였습니다.

---
+++ src/models/Instance.js
@@ -0,0 +1,14 @@
+import 'dotenv/config'
+import { Sequelize } from 'sequelize'
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+    }
+)
+
+export { sequelize }
