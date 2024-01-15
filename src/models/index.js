43b00cb73587ca4c19e499af3680b3f1412e3ba5
xEzIxX <by1094@gachon.ac.kr>
2024-01-15 22:35:00 +0900

    feat : DB와 모델 인스턴스 간의 연결을 하는 index.js파일 수정 1. User, Board, Comment 테이블 순으로 코드를 작성 순서를 변경하였습니다. 2. foreignKey 문자열값의 오타 수정하였습니다. 3. user, board, comment 객체를 불러올 때 매개변수를 삭제하였으므로 그에 맞게 코드를 수정하였습니다.

--- src/models/index.js
+++ src/models/index.js
@@ -1,49 +1,41 @@
-import { Sequelize, Datatypes } from 'sequelize'
+import { Sequelize } from 'sequelize'
 import { sequelize } from './Instance.js'
 
 import { User } from './user.js'
 import { Board } from './board.js'
 import { Comment } from './comment.js'
 
-try{
-const db = {} //실제 db가 이 객체와 연결
+const db = {} // 실제 db가 이 객체와 연결됨
 
 db.sequelize = sequelize
 db.Sequelize = Sequelize
 
-db.User = User(sequelize, DataTypes)
-db.Comment = Comment(sequelize, DataTypes)
-db.Board = Board(sequelize, DataTypes)
-
-/*
-db.User.init(sequelize) // 모델과 테이블의 종합적인 연결
-db.Comment.init(sequelize)
-db.Board.init(sequelize) 
-*/
+db.User = User()
+db.Board = Board()
+db.Comment = Comment()
 
 db.User.hasMany(db.Comment, {
     foreignKey: 'comment_user_id',
 })
 
-db.User.hasMany(db.Board, {
-    foreignKey: 'comment_board_id',
+db.Comment.belongsTo(db.User, {
+    foreignKey: 'comment_user_id',
 })
 
 db.Board.hasMany(db.Comment, {
     foreignKey: 'comment_board_id',
 })
 
-db.Board.belongsTo(db.User, {
-    foreignKey: 'comment_board_id',
-})
 db.Comment.belongsTo(db.Board, {
     foreignKey: 'comment_board_id',
 })
-db.Comment.belongsTo(db.User, {
-    foreignKey: 'comment_user_id',
+
+db.User.hasMany(db.Board, {
+    foreignKey: 'board_user_id',
 })
-}catch(err){
-    console.log('데이터 베이스 정의 실패', err)
-}
+
+db.Board.belongsTo(db.User, {
+    foreignKey: 'board_user_id',
+})
 
 export { db }
