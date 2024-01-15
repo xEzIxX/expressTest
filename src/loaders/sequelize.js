6d52166718e6d34fa7d9ba2e54cd2cfa8e97df60
xEzIxX <by1094@gachon.ac.kr>
2024-01-15 16:43:38 +0900

    feat : loaders/sequelize.js에 시퀄라이즈 인스턴스 생성 부분 삭제

--- src/loaders/sequelize.js
+++ src/loaders/sequelize.js
@@ -1,22 +1,13 @@
-import { Sequelize } from 'sequelize'
+import { sequelize } from '../models/Instance.js'
 
 const sequelizeLoader = async function () {
-    try {
-        const sequelize = new Sequelize(
-            process.env.DB_NAME,
-            process.env.DB_USER,
-            process.env.DB_PASSWORD,
-            {
-                host: process.env.DB_HOST,
-                dialect: process.env.DB_DIALECT,
-            }
-        )
-
+    try{
         await sequelize.authenticate()
         console.log('데이터 베이스 연결 성공')
 
         await sequelize.sync({ force: false })
         console.log('데이터 베이스 동기화 성공')
+
     } catch (error) {
         console.error('데이터 베이스 연결 실패:', error)
     }
