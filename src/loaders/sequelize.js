1ed278a6cd37760622f19b70c94562dddbc817ae
xEzIxX <by1094@gachon.ac.kr>
2024-01-15 21:02:24 +0900

    feat : DB 연결/동기화가 정의된 파일 수정 1. model 디렉토리에서 Sequelize가 아닌 db 객체를 가져와서, 연결 및 동기화 메소드를 사용하도록 수정하였습니다.

--- src/loaders/sequelize.js
+++ src/loaders/sequelize.js
@@ -1,13 +1,12 @@
-import { sequelize } from '../models/Instance.js'
+import { db } from '../models/index.js'
 
 const sequelizeLoader = async function () {
-    try{
-        await sequelize.authenticate()
+    try {
+        await db.sequelize.authenticate()
         console.log('데이터 베이스 연결 성공')
 
-        await sequelize.sync({ force: false })
+        await db.sequelize.sync({ force: true })
         console.log('데이터 베이스 동기화 성공')
-
     } catch (error) {
         console.error('데이터 베이스 연결 실패:', error)
     }
