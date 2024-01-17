45ae851152e97ac082ceceb6ce5a0dbe97324e4b
xEzIxX <by1094@gachon.ac.kr>
2024-01-18 00:28:12 +0900

    feat : 데이터 베이스 동기화시 변경 사항만 확인, 수정하도록 수정 1. db.sequelize.sync({force : true})는  테이블이 생성되고 이미 있는 경우 먼저 삭제하는 방식이었음,  테이블의 변경 사항을 확인하고, 변경 사항만 수정하도록 {alter : true}로 수정

--- src/loaders/sequelize.js
+++ src/loaders/sequelize.js
@@ -5,7 +5,7 @@ export const sequelizeLoader = async function () {
         await db.sequelize.authenticate()
         console.log('데이터 베이스 연결 성공')
 
-        await db.sequelize.sync({ force: true })
+        await db.sequelize.sync({ alter: true })
         console.log('데이터 베이스 동기화 성공')
     } catch (error) {
         console.error('데이터 베이스 연결 실패:', error)
