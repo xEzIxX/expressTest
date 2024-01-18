ada537675f3bad8031fc020ee640494f311d1561
xEzIxX <by1094@gachon.ac.kr>
2024-01-18 11:11:32 +0900

    feat : 비밀번호 암호화 유틸리티 함수 추가

---
+++ src/utils/crypto.js
@@ -0,0 +1,7 @@
+import crypto from 'crypto'
+
+export function createHash(password) {
+    const hash = crypto.createHash('sha512') // 'sha512'알고리즘을 사용하여 hash obj 생성, 반환
+    hash.update(password) // password를 암호화
+    return hash.digest('base64') // 'base64' 인코딩 방식 사용
+}
