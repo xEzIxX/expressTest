ece4eef71f17122c1f95e08a0c4f3a15a17689aa
xEzIxX <by1094@gachon.ac.kr>
2024-01-19 19:33:48 +0900

    refactor : 환경이 설정되지 않은 경우를 고려하여 코드 수정

--- src/config/env.js
+++ src/config/env.js
@@ -1,3 +1,11 @@
 import dotenv from 'dotenv'
-const path = process.env.NODE_ENV === 'development' ? '.env.dev' : '.env.prod'
+
+if (process.env.NODE_ENV === 'development') {
+    path = '.env.dev';
+} else if (process.env.NODE_ENV === 'production') {
+    path = '.env.prod';
+} else {
+    console.log('환경 설정이 되지 않았습니다.')
+}
+
 dotenv.config({ path })
