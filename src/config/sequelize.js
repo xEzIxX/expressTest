df098382b4a51f0b7de0535328d3c882a5adbb8e
xEzIxX <by1094@gachon.ac.kr>
2024-01-19 12:04:55 +0900

    refactor : 환경 확인 및 설정 코드 분리에 따른 sequelize.js파일 수정

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,34 +1,13 @@
-import dotenv from 'dotenv'
+import './env.js'
 
-export function config() {
-    let config
-
-    if (process.env.NODE_ENV === 'production') {
-        dotenv.config({ path: '.env.prod' })
-
-        config = {
-            database: process.env.DB_NAME || 'database',
-            username: process.env.DB_USER || 'root',
-            password: process.env.DB_PASSWORD || 'password',
-            options: {
-                host: process.env.DB_HOST || 'localhost',
-                dialect: process.env.DB_DIALECT,
-            },
-        }
-    } else if (process.env.NODE_ENV === 'development') {
-        dotenv.config({ path: '.env.dev' })
-
-        config = {
-            database: process.env.DB_NAME || 'database',
-            username: process.env.DB_USER || 'root',
-            password: process.env.DB_PASSWORD || 'password',
-            options: {
-                host: process.env.DB_HOST || 'localhost',
-                dialect: process.env.DB_DIALECT,
-            },
-        }
-    } else {
-        console.log('환경 세팅 실패')
-    }
-    return config
+const config = {
+    database: process.env.DB_NAME || 'database',
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'password',
+    options: {
+        host: process.env.DB_HOST || 'localhost',
+        dialect: process.env.DB_DIALECT,
+    },
 }
+
+export { config }
