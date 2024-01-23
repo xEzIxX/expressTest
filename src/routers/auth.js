9d2c252267bba4da8dc45457a2161f6f3ab18d41
xEzIxX <by1094@gachon.ac.kr>
2024-01-23 15:50:24 +0900

    refactor : 로그인,회원가입 라우트 수정 및 새로고침 라우트 추가 1.로그인 라우트('/') 수정 : 사용자에게 받아온 email,pw를 로그인 서비스 함수의 매개변수에 전달,   로그인이 성공하면 서비스 함수에서 반환된 액세스 토큰을 응답 헤더에 추가하고 성공 메시지를 JSON으로 응답 2. 회원가입 라우트('/sign') 수정 : 사용자에게 입력받은 정보를 newUserDto객체에 저장, 이 객체를 회원가입 서비스함수에 전달 3. 새로고침 라우트('/refresh) 추가

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -4,44 +4,48 @@ import { AuthService } from '../services/auth.js'
 const authRouter = express.Router()
 const authService = new AuthService()
 
-authRouter.get('/', function (req, res) {
-    try {
-        authService.login()
-    } catch (err) {
-        console.log(err)
-    }
-})
-
 authRouter.post('/', function (req, res) {
+    // 로그인 페이지
     try {
-        authService.checkLog()
+        const { user_email, password } = req.body // 사용자는 user_email, pw를 통해 로그인
+
+        const accessTK = authService.login(user_email, password) // 로그인 성공 시 tk 발급, 실패시 false
+
+        // Response > Header > Authorization 필드 에 토큰을 담아보내기
+        res.setHeader('Authorization', `Bearer ${accessTK}`)
+
+        return res.status(201).json({
+            code: 201,
+            message: '로그인 성공',
+        })
     } catch (err) {
-        console.log(err)
+        return res.status(401).json({ error: '로그인 실패' })
     }
 })
 
-authRouter.get('/logout', function (req, res) {
+authRouter.post('/sign', async function (req, res) {
     try {
-        authService.main()
+        const newUserDto = {
+            // 사용자에게 정보를 입력 받음
+            user: req.body.user,
+            email: req.body.email,
+            password: req.body.password,
+            name: req.body.name,
+            nickName: req.body.nickname,
+        }
+        const createdUser = await authService.signUp(newUserDto) // 데이터 베이스에 유저 객체 생성
+
+        res.send(createdUser)
     } catch (err) {
-        console.log(err)
+        return res.status(500).json({ error: '회원가입 실패' })
     }
 })
 
-authRouter.get('/sign', function (req, res) {
-    try {
-        authService.signUp()
-    } catch (err) {
-        console.log(err)
-    }
-})
-
-authRouter.post('/sign', function (req, res) {
-    try {
-        authService.signUpHandler()
-    } catch (err) {
-        console.log(err)
-    }
+authRouter.get('/refresh', function (req, res) {
+    // 토큰 만료 혹은 새로고침할 시 이 경로로 redirect
+    /* 1. accessTK 만료, refresh토큰 만료 -> 다시 로그인
+       2. access TK 만료, refresh토큰 만료X -> refresh토큰으로 다시 accessTK 재발급
+       3. access TK 만료 X -> 접근 허용 */
 })
 
 export { authRouter }
