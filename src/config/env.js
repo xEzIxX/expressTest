3d12a74152d69c3715ea747ebc4132a1edba6cc6
xEzIxX <by1094@gachon.ac.kr>
2024-01-23 13:24:29 +0900

    style : 세미콜론 삭제

--- src/config/env.js
+++ src/config/env.js
@@ -1,9 +1,9 @@
 import dotenv from 'dotenv'
 
 if (process.env.NODE_ENV === 'development') {
-    path = '.env.dev';
+    path = '.env.dev'
 } else if (process.env.NODE_ENV === 'production') {
-    path = '.env.prod';
+    path = '.env.prod'
 } else {
     console.log('환경 설정이 되지 않았습니다.')
 }
