271023a7f16f6c0e1f8665f57ee28dcc970c7891
xEzIxX <by1094@gachon.ac.kr>
2024-01-24 16:15:25 +0900

    refactor : routers/auth.js 수정 1. wrapper 함수를 사용하여 async 메소드에서 발생한 에러 처리 2. 로그인 라우터 ('/')   2-(1). login 서비스 함수에 await를 사용하여 비동기 작업이 완료될 때까지 대기   2-(2). login 서비스 함수가 반환하는 객체를 access/refresh 토큰 변수에 비구조화 할당          (이전에는 accessTK만 반환했지만, login서비스 함수 수정으로 accessTK, refreshTK를 포함하는 객체를 반환)   2-(3). 헤더에 refresh토큰 담아 보내는 코드 추가   2-(4). 토큰 발급 실패 여부를 나타내는 if문 추가 3. 회원가입 라우터('/sign')   3-(1). 사용자에게 입력받는 칼럼 중 추가한 이유를 모르겠는 'user:req.body.user' 삭제

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,45 +1,66 @@
 import express from 'express'
+import { wrapper } from '../utils/wrapper.js'
 import { AuthService } from '../services/auth.js'
 
 const authRouter = express.Router()
 const authService = new AuthService()
 
-authRouter.post('/', function (req, res) {
-    // 로그인 페이지
-    try {
-        const { user_email, password } = req.body // 사용자는 user_email, pw를 통해 로그인
+authRouter.post(
+    '/',
+    wrapper(async (req, res) => {
+        // 로그인 페이지
+        try {
+            const { email, password } = req.body // 사용자는 user_email, pw를 통해 로그인
 
-        const accessTK = authService.login(user_email, password) // 로그인 성공 시 tk 발급, 실패시 false
+            const { accessToken, refreshToken } = await authService.login(
+                email,
+                password
+            ) // 로그인 성공 시 tk 발급
 
-        // Response > Header > Authorization 필드 에 토큰을 담아보내기
-        res.setHeader('Authorization', `Bearer ${accessTK}`)
+            console.log(accessToken)
+            // Response > Header > Authoriczation 필드에 토큰을 담아보내기
+            res.setHeader('Authorization', `Bearer ${accessToken}`)
+            res.setHeader('Refresh-Token', refreshToken)
 
-        return res.status(201).json({
-            code: 201,
-            message: '로그인 성공',
-        })
-    } catch (err) {
-        return res.status(401).json({ error: '로그인 실패' })
-    }
-})
+            if (!(accessToken && refreshToken)) {
+                throw new Error('토큰 발급 실패')
+            }
 
-authRouter.post('/sign', async function (req, res) {
-    try {
-        const newUserDto = {
-            // 사용자에게 정보를 입력 받음
-            user: req.body.user,
-            email: req.body.email,
-            password: req.body.password,
-            name: req.body.name,
-            nickName: req.body.nickname,
+            return res.status(201).json({
+                code: 201,
+                message: '로그인 성공',
+            })
+        } catch (err) {
+            return res
+                .status(401)
+                .json({ error: '로그인 실패', errorMessage: err.message })
         }
-        const createdUser = await authService.signUp(newUserDto) // 데이터 베이스에 유저 객체 생성
+    })
+)
 
-        res.send(createdUser)
-    } catch (err) {
-        return res.status(500).json({ error: '회원가입 실패' })
-    }
-})
+authRouter.post(
+    '/sign',
+    wrapper(async (req, res) => {
+        try {
+            const newUserDto = {
+                // 사용자에게 정보를 입력 받음
+                email: req.body.email,
+                password: req.body.password,
+                name: req.body.name,
+                nickName: req.body.nickname,
+            }
+
+            const createdUser = await authService.signUp(newUserDto) // 데이터 베이스에 유저 객체 생성
+
+            return res.status(200).send(createdUser)
+        } catch (err) {
+            console.error(err)
+            return res
+                .status(500)
+                .json({ error: '회원가입 실패', errorMessage: err.message })
+        }
+    })
+)
 
 authRouter.get('/refresh', function (req, res) {
     // 토큰 만료 혹은 새로고침할 시 이 경로로 redirect
