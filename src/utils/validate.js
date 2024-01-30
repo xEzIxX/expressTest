3583aabec5e7e1ab6ad2508d0e2c46e785078b43
xEzIxX <by1094@gachon.ac.kr>
2024-01-30 10:29:09 +0900

    refactor : 유효성 검사 오류에 대한 응답을 처리하는 코드 추가

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,7 +1,7 @@
 import { validationResult } from 'express-validator'
 
 export const validate = validations => {
-    return async (req, res, err, next) => {
+    return async (req, res, next) => {
         for (const validation of validations) {
             const result = await validation.run(req)
             if (result.errors.length) break
@@ -10,8 +10,11 @@ export const validate = validations => {
         const errors = validationResult(req)
         if (errors.isEmpty()) {
             return next()
-        } else {
-            throw new Error(err)
         }
+
+        res.status(400).json({
+            message: '입력값이 올바르지 않습니다.',
+            errors: errors.array(),
+        })
     }
 }
