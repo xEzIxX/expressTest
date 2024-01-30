e9cd3d6aaf59c6463f8fa7487b5039e27dded99d
xEzIxX <by1094@gachon.ac.kr>
2024-01-30 10:30:49 +0900

    refactor : utils/jwt.js의 access 토큰 유효 시간 설정 및 refreshTK 발급 및 유효성 검증 메소드 삭제

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -5,7 +5,8 @@ export class jwtAuth {
     newToken(userId) {
         try {
             return jwt.sign({ id: userId }, process.env.SECRET_AK_KEY, {
-                algorithm: 'HS512', // SHA512
+                algorithm: 'HS512',
+                expiresIn: '30m', // SHA512
             }) // 동기적으로 실행, 문자열 jwt를 발급
         } catch (err) {
             throw new Error('access 토큰 발급 실패')
@@ -13,35 +14,13 @@ export class jwtAuth {
     }
 
     verifyToken(token) {
-        // access token 유효성 검사
+        // access token 유효성 검사,
+        // 유효하면 payload반환, 아니라면 throw err
 
         try {
             return jwt.verify(token, process.env.SECRET_AK_KEY)
         } catch (err) {
             throw new Error('access 토큰 유효성 검사 실패')
-        }
-    }
-
-    refreshToken() {
-        // refresh token 발급
-        try {
-            return jwt.sign({}, process.env.SECRET_FK_KEY, {
-                // refresh token은 payload 없이 발급
-                expiresIn: '14d', // 보통 refresh token의 유효기간은 2주로 설정
-                algorithm: 'HS512', // SHA512
-            })
-        } catch (err) {
-            throw new Error('refresh 토큰 발급 실패')
-        }
-    }
-
-    async refreshVerify(token) {
-        // refresh token 유효성 검증
-
-        try {
-            return jwt.verify(token, process.env.SECRET_FK_KEY)
-        } catch (err) {
-            throw new Error('refresh token 유효성 검사 실패')
         }
     }
 }
