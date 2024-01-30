59b9825fab4a8d50f145b7382007c2405840f42f
xEzIxX <by1094@gachon.ac.kr>
2024-01-30 11:22:12 +0900

    refactor : routers/auth.js 인증 라우터 수정 1. 로그인, 회원가입 라우터    (1) express-validator를 사용힌 valiate 함수를 통해 사용자의 입력에 대한 유효성 검증    (2) catch(err) 구문에서 throw err를 하도록 수정
    2. 로그인 라우터
       (1) post요청 경로 수정 '/'->'/login'
       (2) refreshTK 삭제에 따른 코드 삭제 및 수정
    
    3. 회원가입 라우터
       (1) 회원가입 서비스 함수의 반환값이 불린값으로 수정됨에 따른 코드 수정
    
    4. 로그아웃 라우터 추가
       사용자 헤더의 Authorization에 토큰이 있다면 빈 문자열로 바꾸어 로그아웃, 로그인페이지로 이동하도록 수정
    
    5. 새로고침 라우터 삭제

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,72 +1,99 @@
 import express from 'express'
 import { wrapper } from '../utils/wrapper.js'
 import { AuthService } from '../services/auth.js'
+import { body } from 'express-validator'
+import { validate } from '../utils/validate.js'
 
 const authRouter = express.Router()
 const authService = new AuthService()
 
 authRouter.post(
-    '/',
+    // 로그인 페이지
+    '/login',
+    validate([body('email').notEmpty().isEmail(), body('password').notEmpty()]),
     wrapper(async (req, res) => {
-        // 로그인 페이지
         try {
             const { email, password } = req.body // 사용자는 user_email, pw를 통해 로그인
 
-            const { accessToken, refreshToken } = await authService.login(
+            const { accessToken } = await authService.login(  // tk 발급
                 email,
                 password
-            ) // 로그인 성공 시 tk 발급
+            )
 
-            console.log(accessToken)
-            // Response > Header > Authoriczation 필드에 토큰을 담아보내기
+            // Response > Header > Authoriczation 필드에 검증된 토큰을 담아보내기
             res.setHeader('Authorization', `Bearer ${accessToken}`)
-            res.setHeader('Refresh-Token', refreshToken)
-
-            if (!(accessToken && refreshToken)) {
-                throw new Error('토큰 발급 실패')
-            }
 
             return res.status(201).json({
-                code: 201,
                 message: '로그인 성공',
             })
         } catch (err) {
-            return res
-                .status(401)
-                .json({ error: '로그인 실패', errorMessage: err.message })
+            throw err
         }
     })
 )
 
 authRouter.post(
+    // 회원가입, 유저 정보 데이터 베이스에 저장
     '/sign',
+
+    validate([
+        body('email').notEmpty().isEmail(),
+        body(
+            'password',
+            '비밀번호는 8자 이상, 대소문자, 숫자, 특수문자를 적어도 하나씩 포함해야합니다.'
+        ).isStrongPassword({
+            minLength: 8,
+            minLowercase: 1,
+            minUppercase: 1,
+            minNumbers: 1,
+            minSymbols: 1,
+        }),
+        body('name').notEmpty(),
+        body('nickname').notEmpty(),
+    ]),
+
     wrapper(async (req, res) => {
         try {
             const newUserDto = {
-                // 사용자에게 정보를 입력 받음
+                // 사용자에게 정보를 입력 받은 정보를 담은 newUserDto 객체 생성
                 email: req.body.email,
                 password: req.body.password,
                 name: req.body.name,
-                nickName: req.body.nickname,
+                nickname: req.body.nickname,
             }
 
-            const createdUser = await authService.signUp(newUserDto) // 데이터 베이스에 유저 객체 생성
+            const isCreated = await authService.signUp(newUserDto) // 데이터 베이스에 유저 객체 생성
 
-            return res.status(200).send(createdUser)
+            if (isCreated.result === false) {
+                res.status(404).send(isCreated)
+            } else {
+                res.status(200).send(isCreated) // 유저 객체 생성 완료(회원가입 성공)
+            }
         } catch (err) {
-            console.error(err)
-            return res
-                .status(500)
-                .json({ error: '회원가입 실패', errorMessage: err.message })
+            throw err
         }
     })
 )
 
-authRouter.get('/refresh', function (req, res) {
-    // 토큰 만료 혹은 새로고침할 시 이 경로로 redirect
-    /* 1. accessTK 만료, refresh토큰 만료 -> 다시 로그인
-       2. access TK 만료, refresh토큰 만료X -> refresh토큰으로 다시 accessTK 재발급
-       3. access TK 만료 X -> 접근 허용 */
-})
+authRouter.get(
+    '/logout',
+    // 로그아웃
+    wrapper(async (req, res) => {
+        try {
+            const Token = req.headers.authorization
+            let isToken
+
+            Token === null ? (isToken = false) : (isToken = true)
+
+            if (isToken) {
+                res.setHeader('Authorization', '')
+                //console.log('로그아웃 성공, 로그인 페이지로 이동합니다.')
+                res.redirect('../login')
+            }
+        } catch (err) {
+            throw err
+        }
+    })
+)
 
 export { authRouter }
