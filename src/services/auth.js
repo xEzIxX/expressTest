e7c051362b9d2a558178a721595dc54de73fdd8d
xEzIxX <by1094@gachon.ac.kr>
2024-01-30 11:24:42 +0900

    refactor : services/auth.js 서비스 함수 수정
    0. catch(err)에서 throw err 하도록 수정
    
    1. 로그인 서비스 함수
        (1) 입력한 이메일, 비밀번호 모두 일치하는 유저를 찾도록 수정
        (2) 토큰의 유효성 검증하는 auth.verifyToken함수 추가
        (3) refreshTK 삭제에 따른 코드 수정
    
    2. 회원가입 서비스 함수
        unique한 칼럼이 중복되지 않도록 검증하고, 그에 따라 다른 반환값을 주도록 수정

--- src/services/auth.js
+++ src/services/auth.js
@@ -6,34 +6,26 @@ export class AuthService {
     async login(user_email, password) {
         // 로그인 함수
         try {
-            if (!user_email || !password) {
-                throw new Error(
-                    '이메일, 혹은 비밀번호가 모두 입력되지 않았습니다.'
-                )
-            }
-
             const hashedPw = getHash(password)
 
             const user = await db.User.findOne({
-                // 입력한 이메일에 맞는 유저 찾기
-                where: { user_email },
+                // 입력한 이메일, 비밀번호에 맞는 유저 찾기
+                where: { user_email, user_pw: hashedPw },
             })
 
-            // 유저가 존재하지 않거나 비밀번호가 올바르지 않은 경우 err
-            if (user === null || !(user.user_pw === hashedPw)) {
-                throw new Error('유저가 존재하지 않거나 비밀번호가 틀림')
+            if (user === null) {
+                throw err
+            } else {
+                // 로그인 정보가 올바른 경우 access TK발급
+                const auth = new jwtAuth()
+                const accessToken = auth.newToken(user.user_id)
+                auth.verifyToken(accessToken) // 유효하면 payload반환, 아니라면 throw err
+                //console.log('payload : ', auth.verifyToken(accessToken))
+
+                return { accessToken }
             }
-
-            const auth = new jwtAuth()
-
-            // 로그인 정보가 올바른 경우 access TK, refresh TK 발급
-            const accessToken = auth.newToken(user.user_id)
-            const refreshToken = auth.refreshToken()
-
-            return { accessToken, refreshToken }
         } catch (err) {
-            console.log(err)
-            throw new Error('로그인 함수 실패')
+            throw err
         }
     }
 
@@ -41,19 +33,34 @@ export class AuthService {
         try {
             const hashedPw = getHash(newUserDto.password)
 
-            const newUser = await db.User.create({
-                // 시퀄라이즈 create를 통해 새로운 유저의 정보 DB에 저장
-                user_email: newUserDto.email,
-                user_pw: hashedPw,
-                user_name: newUserDto.name,
-                user_nickname: newUserDto.email,
+            const existEmail = await db.User.findOne({
+                where: { user_email: newUserDto.email },
             })
+            // console.log(existEmail)
+            const existNickname = await db.User.findOne({
+                where: { user_nickname: newUserDto.nickname },
+            })
+            // console.log(existNickname)
 
-            console.log(newUser)
-            return newUser
+            if (existEmail === null && existNickname === null) {
+                await db.User.create({
+                    // 시퀄라이즈 create를 통해 새로운 유저의 정보 DB에 저장
+                    user_email: newUserDto.email,
+                    user_pw: hashedPw,
+                    user_name: newUserDto.name,
+                    user_nickname: newUserDto.nickname,
+                })
+
+                return { result: true, message: '회원가입' }
+            } else if (existEmail === null && existNickname !== null) {
+                return { result: false, message: '중복된 닉네임' }
+            } else if (existEmail !== null && existNickname === null) {
+                return { result: false, message: '중복된 이메일' }
+            } else {
+                return { result: false, message: '중복된 닉네임과 이메일' }
+            }
         } catch (err) {
-            console.error(err)
-            throw new Error('DB에 정보 저장 실패')
+            throw err
         }
     }
 }
