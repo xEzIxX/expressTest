7723269ff3909f9dc4bfb28f61d94d751b37276b
xEzIxX <by1094@gachon.ac.kr>
2024-02-04 19:46:47 +0900

    refactor : services/auth.js 수정 1. 로그인 메소드   (1). 매개변수로 userDto를 받아오도록 수정   (2). findOne으로 받아온 user가 존재하지 않을 경우 뿐만 아닌 비정상적일 경우 처리하는 로직 추가(undefined)   (3). utils/jwt.js 파일 수정에 따른 코드 수정
    2. 회원가입 메소드
      (1). 입력한 비밀번호와 비밀번호 확인을 위해 입력한 비밀번호가 일치하는 지 처리하는 로직 추가
      (2). 입력한 닉네임, 이메일이 중복되는 지 케이스를 나누어 다른 message를 할당하는 로직 추가

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,25 +1,31 @@
-import { getHash } from '../utils/crypto.js'
-import { jwtAuth } from '../utils/jwt.js'
+import '../config/env.js'
+import jwt from 'jsonwebtoken'
+import { newToken } from '../utils/newToken.js'
+import { getHash } from '../utils/getHash.js'
 import { db } from '../models/index.js'
 
 export class AuthService {
-    async login(user_email, password) {
+    async login(userDto) {
         // 로그인 함수
         try {
-            const hashedPw = getHash(password)
+            const hashedPw = getHash(userDto.password)
 
             const user = await db.User.findOne({
                 // 입력한 이메일, 비밀번호에 맞는 유저 찾기
-                where: { user_email, user_pw: hashedPw },
+                where: { user_email: userDto.email, user_pw: hashedPw },
             })
 
-            if (user === null) {
+            if (user === null) { // 존재하지 않음
+                throw err
+            } else if (user === undefined) { // 비정상적인 case
                 throw err
             } else {
                 // 로그인 정보가 올바른 경우 access TK발급
-                const auth = new jwtAuth()
-                const accessToken = auth.newToken(user.user_id, user.user_nickname)
-                auth.verifyToken(accessToken) // 유효하면 복호화된 payload반환, 아니라면 throw err
+                const accessToken = newToken(
+                    user.user_id,
+                    user.user_nickname
+                )
+                jwt.verify(accessToken, process.env.SECRET_AK_KEY) // 유효하지 않으면 throw err
 
                 return { accessToken }
             }
@@ -30,6 +36,13 @@ export class AuthService {
 
     async signUp(newUserDto) {
         try {
+            if (newUserDto.password !== newUserDto.checkPw) {
+                return {
+                    result: false,
+                    message:
+                        '비밀번호 확인 칸에는 동일한 비밀번호를 입력해주세요.',
+                }
+            }
             const hashedPw = getHash(newUserDto.password)
 
             const existEmail = await db.User.findOne({
@@ -51,12 +64,16 @@ export class AuthService {
                 })
 
                 return { result: true, message: '회원가입' }
-            } else if (existEmail === null && existNickname !== null) {
-                return { result: false, message: '중복된 닉네임' }
-            } else if (existEmail !== null && existNickname === null) {
-                return { result: false, message: '중복된 이메일' }
             } else {
-                return { result: false, message: '중복된 닉네임과 이메일' }
+                let falseMessage
+
+                if (existEmail === null && existNickname !== null) {
+                    falseMessage = '중복된 닉네임'
+                } else if (existEmail !== null && existNickname === null) {
+                    falseMessage = '중복된 이메일'
+                } else falseMessage = '중복된 닉네임과 이메일'
+
+                return { result: false, message: falseMessage }
             }
         } catch (err) {
             throw err
