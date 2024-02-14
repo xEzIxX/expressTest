07e20ac3b67e4a0d27ac5a7424028ca946aea226
xEzIxX <by1094@gachon.ac.kr>
2024-02-14 16:30:48 +0900

    refactor : SECRET_AK_KEY -> SECRET_KEY 로 수정

--- src/utils/newToken.js
+++ src/utils/newToken.js
@@ -2,17 +2,13 @@ import '../config/env.js'
 import jwt from 'jsonwebtoken'
 
 export function newToken(userId, userNickname) {
-        // payload에 사용자의 id, nickname을 넣어준다.
-        try {
-            return jwt.sign(
-                { userId: userId, userNickname: userNickname },
-                process.env.SECRET_AK_KEY,
-                {
-                    algorithm: 'HS512',
-                    expiresIn: '30m', // SHA512
-                }
-            ) // 동기적으로 실행, 문자열 jwt를 발급
-        } catch (err) {
-            throw new Error('access 토큰 발급 실패')
-        }
+    // payload에 사용자의 id, nickname을 넣어준다.
+    try {
+        return jwt.sign({ userId, userNickname }, process.env.SECRET_KEY, {
+            algorithm: 'HS512',
+            expiresIn: '30m', // SHA512
+        }) // 동기적으로 실행, 문자열 jwt를 발급
+    } catch (err) {
+        throw new Error('access 토큰 발급 실패')
+    }
 }
