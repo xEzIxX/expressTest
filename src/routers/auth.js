1cd3823048bed1a7e74776e27d19dc49c8fefe9f
xEzIxX <by1094@gachon.ac.kr>
2024-02-14 16:19:04 +0900

    refactor : 인증 라우터 수정 1. 로그인, 회원가입 조회 라우터 추가 2. res.send()값을 객체의 속성에서 객체로 변경, 로그인 라우터에서는 HTTP 상태 코드도 함께 반환하도록 수정 3. 서비스 함수에서 반환되는 불린값을 if, else if, else를 이용하여 T/F 이외의 상황도 처리하도록 수정 4. export 위치 수정

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -4,11 +4,15 @@ import { AuthService } from '../services/auth.js'
 import { body } from 'express-validator'
 import { validate } from '../utils/validate.js'
 
-const authRouter = express.Router()
+export const authRouter = express.Router()
 const authService = new AuthService()
 
+// 로그인 페이지
+authRouter.get('/login', (req, res) => {
+    return res.render('login.ejs')
+})
+
 authRouter.post(
-    // 로그인 페이지
     '/login',
     validate([body('email').notEmpty().isEmail(), body('password').notEmpty()]),
     wrapper(async (req, res) => {
@@ -20,23 +24,33 @@ authRouter.post(
 
             const foundUser = await authService.login(userDto) // tk 발급
 
-            if (foundUser.result === true){
-                const accessToken = foundUser.accessToken
+            if (foundUser.result === true) {
+                const accessToken = foundUser.token
                 res.setHeader('Authorization', `Bearer ${accessToken}`)
 
-                return res.send(foundUser.message)
-            }else{
-                return res.send(foundUser.message)
+                return res.status(200).send(foundUser)
+            } else if (foundUser.result === false) {
+                return res.status(401).send(foundUser) // 잘못된 회원 정보 입력
+            } else {
+                throw err
             }
-
         } catch (err) {
+            console.log(err)
             throw err
         }
     })
 )
 
+// 회원가입
+authRouter.get('/sign', (req, res) => {
+    try {
+        return res.render('sign.ejs')
+    } catch {
+        throw err
+    }
+})
+
 authRouter.post(
-    // 회원가입
     '/sign',
 
     validate([
@@ -76,9 +90,11 @@ authRouter.post(
             const newUser = await authService.signUp(newUserDto) // 데이터 베이스에 유저 객체 생성
 
             if (newUser.result === true) {
-                return res.status(201).send(newUser.message) // 유저 객체 생성 완료(회원가입 성공)
+                return res.status(201).send(newUser) // 유저 객체 생성 완료(회원가입 성공)
+            } else if (newUser.result === false) {
+                return res.status(404).send(newUser)
             } else {
-                return res.status(404).send(newUser.message)
+                throw err
             }
         } catch (err) {
             throw err
@@ -86,9 +102,9 @@ authRouter.post(
     })
 )
 
+// 로그아웃
 authRouter.get(
     '/logout',
-    // 로그아웃
     wrapper(async (req, res) => {
         try {
             const token = req.headers.authorization
@@ -99,16 +115,16 @@ authRouter.get(
                 token === ''
             )
 
-            if (isToken) {
+            if (isToken === true) {
                 res.setHeader('Authorization', '')
                 return res.redirect('../auth/login')
-            } else {
+            } else if (isToken === false) {
                 return res.send('로그인 상태가 아닙니다.')
+            } else {
+                throw err
             }
         } catch (err) {
             throw err
         }
     })
 )
-
-export { authRouter }
