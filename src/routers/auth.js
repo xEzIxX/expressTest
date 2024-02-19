e7b4565ea69d437a92a2fc378114968be1ece778
xEzIxX <by1094@gachon.ac.kr>
2024-02-19 12:20:42 +0900

    refactor : 로그아웃 라우터에서 '문자열->객체 형식'으로 응답하도록 변경

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -110,11 +110,11 @@ authRouter.get(
 
             if (isToken === true) {
                 res.setHeader('Authorization', '')
-                return res.status(200).send({message : '로그아웃 성공'}) /// 'auth/login' 경로로 이동
+                return res.status(200).send({message : '로그아웃 성공'})
             } else if (isToken === false) {
-                return res.status(401).send('로그인 상태가 아님')
+                return res.status(401).send({message : '로그인 상태가 아님'})
             } else {
-                return res.status(500).send('로그아웃 실패')
+                return res.status(500).send({message : '로그아웃 실패'})
             }
         } catch (err) {
             throw err
