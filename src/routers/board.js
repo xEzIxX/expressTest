ba55787aa581429f0039c8ec6e36bda7de5a2fc0
xEzIxX <by1094@gachon.ac.kr>
2024-02-20 21:12:18 +0900

    feat : 게시글 리스트 및 검색 결과 응답 라우터 함수 추가

--- src/routers/board.js
+++ src/routers/board.js
@@ -11,6 +11,37 @@ import { isPasswordValid } from '../utils/isPasswordValid.js'
 export const boardRouter = express.Router()
 const boardService = new BoardService()
 
+boardRouter.get(
+    '/', // 리스트조회
+    wrapper(async (req, res) => {
+        try {
+            const allBoard = await boardService.foundAllBoard() // 모든 게시글 반환 서비스 함수
+            // return res.send(allBoard)
+            return res.render('board/list.ejs', allBoard)
+        } catch (err) {
+            throw err
+        }
+    })
+)
+
+boardRouter.get(
+    // 검색
+    '/search',
+    wrapper(async (req, res) => {
+        try {
+            const queryDto = {
+                q: req.query.q,
+                sort: req.query.sort,
+            }
+            const searchedResult = await boardService.searchList(queryDto)
+            // return res.send(searchedResult)
+            return res.render('board/list.ejs', searchedResult)
+        } catch (err) {
+            throw err
+        }
+    })
+)
+
 /* [ /board/form ] 게시글 작성 */
 
 boardRouter.get(
@@ -61,19 +92,20 @@ boardRouter.get(
     '/:boardId/edit',
     wrapper(async (req, res) => {
         try {
-            const boardId = req.params.boardId
-            const foundOriginal = await boardService.getOriginalBoardById(boardId)
+            const boardId = req.params.boardId.split(':')[1]
+            const foundOriginal =
+                await boardService.getOriginalBoardById(boardId)
 
             const token = req.headers.authorization.split(' ')[1]
             const decoded = jwt.verify(token, process.env.SECRET_KEY)
 
-            //console.log('decoded.userId : ', decoded.userId)
-            //console.log('foundOriginal.data.board_user_id : ',foundOriginal.data.board_user_id)
+            // console.log('***decoded.userId : ', decoded.userId)
+            // console.log('***foundOriginal.board_user_id : ',foundOriginal.data.board_user_id)
 
             const isValid = decoded.userId === foundOriginal.data.board_user_id
 
             if (isValid) {
-                //return res.status(200).send(foundOriginal)
+                // return res.status(200).send(foundOriginal)
                 return res.status(200).render('board/edit.ejs', foundOriginal)
             } else {
                 return res.status(401).send({ message: '권한없음' })
@@ -123,7 +155,7 @@ boardRouter.get(
             const board = await boardService.getBoardById(boardId) // 게시글 아이디 boardId와 일치하는 글 갖고옴
 
             if (board.result === true) {
-                //return res.status(200).send(board)
+                // return res.status(200).send(board)
                 return res.status(200).render('board', { boardId, data: board }) // 그 글 페이지의 데이터, 화면을 보여줘야함
             } else {
                 return res.status(404).render('board', { boardId, data: board })
@@ -138,15 +170,13 @@ boardRouter.delete(
     // 게시글 아이디가 boardId인 게시글 삭제
     '/:boardId',
     validate([
-        body(
-            'password',
-        ).isStrongPassword({
+        body('password').isStrongPassword({
             minLength: 8,
             minLowercase: 1,
             minUppercase: 1,
             minNumbers: 1,
             minSymbols: 1,
-        })
+        }),
     ]),
     wrapper(async (req, res) => {
         try {
