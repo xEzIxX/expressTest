443c1310a516a3116c7931d506f9104f1dd5360a
xEzIxX <by1094@gachon.ac.kr>
2024-02-20 21:06:41 +0900

    feat : 게시글 리스트 및 검색 결과 반환 서비스 함수

--- src/services/board.js
+++ src/services/board.js
@@ -1,6 +1,75 @@
 import { db } from '../models/index.js'
+import { Op } from 'sequelize'
 
 export class BoardService {
+    async foundAllBoard() {
+        try {
+            const allBoard = await db.Board.findAndCountAll() // 모델인스턴스의 배열 반환
+
+            if (allBoard.count > 0) {
+                return {
+                    result: true,
+                    message: '게시글 조회 성공',
+                    data: allBoard.rows,
+                }
+            } else {
+                return {
+                    result: false,
+                    message: '게시글 조회 실패',
+                    data: null,
+                }
+            }
+        } catch (err) {
+            throw err
+        }
+    }
+
+    async searchList(query) {
+        try {
+            const sort = query.sort
+            let sortOrder
+
+            if (sort === 'date_asc') sortOrder = ['createdAt', 'ASC']
+            else if (sort === 'date_desc') sortOrder = ['createdAt', 'DESC']
+            else if (sort === 'likes_asc') sortOrder = ['board_like', 'ASC']
+            else if (sort === 'likes_desc') sortOrder = ['board_like', 'DESC']
+            else if (sort === 'views_asc') sortOrder = ['board_view', 'ASC']
+            else if (sort === 'view_desc') sortOrder = ['board_view', 'DESC']
+            else {
+                return {
+                    result: false,
+                    message: '잘못된 정렬 방식',
+                    data: null,
+                }
+            }
+
+            const searchedBoard = await db.Board.findAndCountAll({
+                where: {
+                    board_title: {
+                        [Op.substring]: query.q, // title에 검색어 포함 여부 확인
+                    },
+                },
+                order: [sortOrder], // 정렬 방식
+            })
+
+            if (searchedBoard.count > 0) {
+                return {
+                    result: true,
+                    message: '검색된 게시글 조회 성공',
+                    data: searchedBoard.rows,
+                }
+            } else {
+                return {
+                    result: false,
+                    message: '검색된 게시글 조회 실패',
+                    data: null,
+                }
+            }
+        } catch (err) {
+            throw err
+        }
+    }
+
     async createNewBoard(boardDto) {
         // 작성된 게시글 저장
         try {
@@ -33,6 +102,8 @@ export class BoardService {
             const original = await db.Board.findOne({
                 where: { board_id: boardId },
             }) // 반환객체 : Board{ dataValues: {}, _previousDataValues: {}, ... }
+
+            // console.log(original.dataValues)
 
             if (original instanceof db.Board) {
                 return {
