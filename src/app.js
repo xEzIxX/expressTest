58b239cbb66da8b66b82650565eaec1ef1fa3aaa
xEzIxX <by1094@gachon.ac.kr>
2024-02-22 01:38:20 +0900

    refactor : app.js에 토큰 검증 미들웨어 추가 및 검증이 필요한 경로에 대한 라우트 추가

--- src/app.js
+++ src/app.js
@@ -2,15 +2,23 @@ import './config/env.js'
 
 import express from 'express'
 import morgan from 'morgan'
+
 import { authRouter } from './routers/auth.js'
 import { boardRouter } from './routers/board.js'
+
 import { sequelizeLoader } from './loaders/sequelize.js'
+import { tokenChecker } from './utils/tokenChecker.js'
+
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use(morgan('dev'))
+
+app.route('/board/form').get(tokenChecker()).post(tokenChecker()) // 게시글 작성 폼 조회get, 저장 post(req.userId도 저장)
+app.route('/board/:boardId/edit').get(tokenChecker()) // 수정 페이지 조회get (조회해야만 저장 가능)
+app.route('/board/:boardId').delete(tokenChecker()) // 게시글 삭제
 
 app.use('/auth', authRouter)
 app.use('/board', boardRouter)
