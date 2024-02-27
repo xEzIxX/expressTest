093af466ebd67d812dc39531c1c8a4f5aae0de77
xEzIxX <by1094@gachon.ac.kr>
2024-02-27 10:55:21 +0900

    refactor : utils/tokenChecker.js에서 오류 처리 로직 수정

--- src/utils/tokenChecker.js
+++ src/utils/tokenChecker.js
@@ -10,7 +10,16 @@ export function tokenChecker() {
             req.userId = decoded.userId
             next()
         } catch (err) {
-            return next(err)
+            if (err.name === 'TokenExpiredError') {
+                return res.status(401).send({ message: '만료된 토큰' })
+            } else if (err.name === 'JsonWebTokenError') {
+                return res.status(401).send({ message: '유효하지 않은 토큰' })
+            } else if (err.name === 'TypeError') {
+                return res.status(401).send({ message: '잘못된 형식의 토큰' })
+                //TypeError: Cannot read properties of undefined (reading 'split')
+            } else {
+                return next(err)
+            }
         }
     }
 }
