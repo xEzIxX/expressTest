b360d81fbe4c515b756405d7b684ace3c8591937
xEzIxX <by1094@gachon.ac.kr>
2024-02-27 10:20:02 +0900

    refactor : app.js 수정 1. 템플릿 엔진의 뷰 파일들이 위치하는 디렉토리를 설정하는 코드 추가 2. 주석 추가 및 코드 포맷팅

--- src/app.js
+++ src/app.js
@@ -11,24 +11,27 @@ import { tokenChecker } from './utils/tokenChecker.js'
 
 const app = express()
 
+app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use(morgan('dev'))
+app.set('view engine', 'ejs')
+app.set('views', './src/views')
 
+await sequelizeLoader()
+
+// 토큰 유무를 판단하는 미들웨어
 app.route('/board/form').get(tokenChecker()).post(tokenChecker()) // 게시글 작성 폼 조회get, 저장 post(req.userId도 저장)
 app.route('/board/:boardId/edit').get(tokenChecker()) // 수정 페이지 조회get (조회해야만 저장 가능)
 app.route('/board/:boardId').delete(tokenChecker()) // 게시글 삭제
 
+// 라우터
 app.use('/auth', authRouter)
 app.use('/board', boardRouter)
 
-app.set('view engine', 'ejs')
-
-await sequelizeLoader()
-
+// 에러 핸들러
 app.use((err, req, res, next) => {
-    console.log(err)
+    // console.log('에러 핸들러에서 처리된 서버 오류', err)
     res.status(500).send({ message: '서버 오류' })
 })
 
