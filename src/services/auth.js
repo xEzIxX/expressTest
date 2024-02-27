389c65df47ce0c27b6ffc8f8ee751ec3dc93f52b
xEzIxX <by1094@gachon.ac.kr>
2024-02-27 11:17:07 +0900

    refactor : 서비스 함수의 try..catch문 삭제

--- src/services/auth.js
+++ src/services/auth.js
@@ -6,105 +6,97 @@ import { db } from '../models/index.js'
 export class AuthService {
     async login(userDto) {
         // 로그인 함수
-        try {
-            const hashedPw = getHash(userDto.password)
 
-            const user = await db.User.findOne({
-                // findOne은 User 모델의 인스턴스 혹은 null을 반환,
-                where: { user_email: userDto.email, user_pw: hashedPw },
-            })
+        const hashedPw = getHash(userDto.password)
 
-            if (user instanceof db.User) {
-                const accessToken = newToken(user.user_id)
+        const user = await db.User.findOne({
+            // findOne은 User 모델의 인스턴스 혹은 null을 반환,
+            where: { user_email: userDto.email, user_pw: hashedPw },
+        })
 
-                if (Boolean(accessToken) === true) {
-                    // access토큰이 정상적으로 발급 되었다면
-                    return {
-                        result: true,
-                        message: '존재하는 회원',
-                        token: accessToken,
-                    }
-                } else {
-                    return {
-                        result: false,
-                        message: '토큰 발행 오류',
-                        token: null,
-                    }
-                }
-            } else if (user === null) {
+        if (user instanceof db.User) {
+            const accessToken = newToken(user.user_id)
+
+            if (Boolean(accessToken) === true) {
+                // access토큰이 정상적으로 발급 되었다면
                 return {
-                    result: false,
-                    message: '존재하지 않는 회원',
-                    token: null,
+                    result: true,
+                    message: '존재하는 회원',
+                    token: accessToken,
                 }
             } else {
-                return { result: false, message: '서비스 오류', token: null }
+                return {
+                    result: false,
+                    message: '토큰 발행 오류',
+                    token: null,
+                }
             }
-        } catch (err) {
-            throw err
+        } else if (user === null) {
+            return {
+                result: false,
+                message: '존재하지 않는 회원',
+                token: null,
+            }
+        } else {
+            return { result: false, message: '서비스 오류', token: null }
         }
     }
 
     async signUp(newUserDto) {
-        try {
-            if (newUserDto.password !== newUserDto.checkPw) {
-                return {
-                    result: false,
-                    message:
-                        '비밀번호 확인 칸에는 동일한 비밀번호를 입력해주세요.',
-                }
+        if (newUserDto.password !== newUserDto.checkPw) {
+            return {
+                result: false,
+                message: '비밀번호 확인 칸에는 동일한 비밀번호를 입력해주세요.',
             }
-            const hashedPw = getHash(newUserDto.password)
+        }
+        const hashedPw = getHash(newUserDto.password)
 
-            const foundEmail = await db.User.findOne({
-                where: { user_email: newUserDto.email },
+        const foundEmail = await db.User.findOne({
+            where: { user_email: newUserDto.email },
+        })
+
+        const foundNickname = await db.User.findOne({
+            where: { user_nickname: newUserDto.nickname },
+        })
+
+        if (foundEmail === null && foundNickname === null) {
+            await db.User.create({
+                // 시퀄라이즈 create를 통해 새로운 유저의 정보 DB에 저장
+                user_email: newUserDto.email,
+                user_pw: hashedPw,
+                user_name: newUserDto.name,
+                user_nickname: newUserDto.nickname,
             })
 
-            const foundNickname = await db.User.findOne({
-                where: { user_nickname: newUserDto.nickname },
-            })
-
-            if (foundEmail === null && foundNickname === null) {
-                await db.User.create({
-                    // 시퀄라이즈 create를 통해 새로운 유저의 정보 DB에 저장
+            const createdResult = await db.User.findOne({
+                where: {
                     user_email: newUserDto.email,
                     user_pw: hashedPw,
                     user_name: newUserDto.name,
                     user_nickname: newUserDto.nickname,
-                })
+                },
+            })
 
-                const createdResult = await db.User.findOne({
-                    where: {
-                        user_email: newUserDto.email,
-                        user_pw: hashedPw,
-                        user_name: newUserDto.name,
-                        user_nickname: newUserDto.nickname,
-                    },
-                })
-
-                if (createdResult instanceof db.User) {
-                    return { result: true, message: '회원가입 완료' }
-                } else if (createdResult === null) {
-                    return {
-                        result: false,
-                        message: '회원가입 실패',
-                    }
-                } else {
-                    return { result: false, message: '서비스 오류' }
+            if (createdResult instanceof db.User) {
+                return { result: true, message: '회원가입 완료' }
+            } else if (createdResult === null) {
+                return {
+                    result: false,
+                    message: '회원가입 실패',
                 }
             } else {
-                let falseMessage
-
-                if (foundEmail === null && foundNickname !== null) {
-                    falseMessage = '중복된 닉네임'
-                } else if (foundEmail !== null && foundNickname === null) {
-                    falseMessage = '중복된 이메일'
-                } else falseMessage = '중복된 닉네임과 이메일'
-
-                return { result: false, falseMessage }
+                return { result: false, message: '서비스 오류' }
             }
-        } catch (err) {
-            throw err
+        } else {
+            let falseMessage
+
+            if (foundEmail === null && foundNickname !== null) {
+                falseMessage = '중복된 닉네임'
+            } else if (foundEmail !== null && foundNickname === null) {
+                falseMessage = '중복된 이메일'
+            } else falseMessage = '중복된 닉네임과 이메일'
+
+            return { result: false, falseMessage }
         }
     }
 }
--- src/services/board.js
+++ src/services/board.js
@@ -3,221 +3,198 @@ import { Op } from 'sequelize'
 
 export class BoardService {
     async findAllBoard() {
-        try {
-            const allBoard = await db.Board.findAndCountAll() // 모델인스턴스의 배열 반환
+        const allBoard = await db.Board.findAndCountAll() // 모델인스턴스의 배열 반환
 
-            if (allBoard.count >= 0) {
-                return {
-                    result: true,
-                    message: '게시글 조회 성공',
-                    data: allBoard.rows,
-                }
-            } else {
-                return {
-                    result: false,
-                    message: '게시글 조회 실패',
-                    data: null,
-                }
+        if (allBoard.count >= 0) {
+            return {
+                result: true,
+                message: '게시글 조회 성공',
+                data: allBoard.rows,
             }
-        } catch (err) {
-            throw err
+        } else {
+            return {
+                result: false,
+                message: '게시글 조회 실패',
+                data: null,
+            }
         }
     }
 
     async searchList(queryDto) {
-        try {
-            let sortOrder
+        let sortOrder
 
-            switch (queryDto.sort) {
-                case 'date_asc':
-                    sortOrder = ['createdAt', 'ASC']
-                    break
-                case 'date_desc':
-                case '': // 빈문자열인 경우 최신순 정렬
-                    sortOrder = ['createdAt', 'DESC']
-                    break
-                case 'likes_asc':
-                    sortOrder = ['board_like', 'ASC']
-                    break
-                case 'likes_desc':
-                    sortOrder = ['board_like', 'DESC']
-                    break
-                case 'views_asc':
-                    sortOrder = ['board_view', 'ASC']
-                    break
-                case 'views_desc':
-                    sortOrder = ['board_view', 'DESC']
-                    break
-                default:
-                    return {
-                        result: false,
-                        message: '잘못된 정렬 방식',
-                        data: null,
-                    }
-            }
-
-            const searchedBoard = await db.Board.findAndCountAll({
-                where: {
-                    board_title: {
-                        [Op.substring]: queryDto.q, // title에 검색어 포함 여부 확인
-                    },
-                },
-                order: [sortOrder], // 정렬 방식
-            })
-
-            if (searchedBoard.count >= 0) {
-                return {
-                    result: true,
-                    message: '검색된 게시글 조회 성공',
-                    data: searchedBoard.rows,
-                }
-            } else {
+        switch (queryDto.sort) {
+            case 'date_asc':
+                sortOrder = ['createdAt', 'ASC']
+                break
+            case 'date_desc':
+            case '': // 빈문자열인 경우 최신순 정렬
+                sortOrder = ['createdAt', 'DESC']
+                break
+            case 'likes_asc':
+                sortOrder = ['board_like', 'ASC']
+                break
+            case 'likes_desc':
+                sortOrder = ['board_like', 'DESC']
+                break
+            case 'views_asc':
+                sortOrder = ['board_view', 'ASC']
+                break
+            case 'views_desc':
+                sortOrder = ['board_view', 'DESC']
+                break
+            default:
                 return {
                     result: false,
-                    message: '검색된 게시글 조회 실패',
+                    message: '잘못된 정렬 방식',
                     data: null,
                 }
+        }
+
+        const searchedBoard = await db.Board.findAndCountAll({
+            where: {
+                board_title: {
+                    [Op.substring]: queryDto.q, // title에 검색어 포함 여부 확인
+                },
+            },
+            order: [sortOrder], // 정렬 방식
+        })
+
+        if (searchedBoard.count >= 0) {
+            return {
+                result: true,
+                message: '검색된 게시글 조회 성공',
+                data: searchedBoard.rows,
             }
-        } catch (err) {
-            throw err
+        } else {
+            return {
+                result: false,
+                message: '검색된 게시글 조회 실패',
+                data: null,
+            }
         }
     }
 
     async createNewBoard(boardDto) {
         // 작성한 게시글 저장
-        try {
-            const createdResult = await db.Board.create({
-                board_title: boardDto.title,
-                board_content: boardDto.content,
-                board_user_id: boardDto.userId,
-            })
 
-            if (createdResult instanceof db.Board) {
-                return {
-                    result: true,
-                    message: '게시글 저장 성공',
-                }
-            } else {
-                return {
-                    result: false,
-                    message: '게시글 저장 실패',
-                }
+        const createdResult = await db.Board.create({
+            board_title: boardDto.title,
+            board_content: boardDto.content,
+            board_user_id: boardDto.userId,
+        })
+
+        if (createdResult instanceof db.Board) {
+            return {
+                result: true,
+                message: '게시글 저장 성공',
             }
-        } catch (err) {
-            throw err
+        } else {
+            return {
+                result: false,
+                message: '게시글 저장 실패',
+            }
         }
     }
 
     async getOriginalBoardById(boardDto) {
         // 게시글 수정을 위해 게시글 원본 반환
-        try {
-            const original = await db.Board.findOne({
-                where: { board_id: boardDto.boardId },
-            }) // 반환객체 : Board{ dataValues: {}, _previousDataValues: {}, ... }
 
-            if (original instanceof db.Board) {
-                return {
-                    result: true,
-                    message: '게시글 반환 성공',
-                    data: original.dataValues,
-                }
-            } else if (original === null) {
-                return {
-                    result: false,
-                    message: '게시글 반환 실패',
-                    data: null,
-                }
-            } else {
-                return {
-                    result: false,
-                    message: '게시글 반환 오류',
-                    data: null,
-                }
+        const original = await db.Board.findOne({
+            where: { board_id: boardDto.boardId },
+        }) // 반환객체 : Board{ dataValues: {}, _previousDataValues: {}, ... }
+
+        if (original instanceof db.Board) {
+            return {
+                result: true,
+                message: '게시글 반환 성공',
+                data: original.dataValues,
             }
-        } catch (err) {
-            throw err
+        } else if (original === null) {
+            return {
+                result: false,
+                message: '게시글 반환 실패',
+                data: null,
+            }
+        } else {
+            return {
+                result: false,
+                message: '게시글 반환 오류',
+                data: null,
+            }
         }
     }
 
     async updateBoardById(boardDto) {
         // 수정된 게시글 데이터 저장
-        try {
-            const result = await db.Board.update(
-                {
-                    board_title: boardDto.title,
-                    board_content: boardDto.content,
-                },
-                {
-                    where: {
-                        board_id: boardDto.boardId,
-                    },
-                }
-            )
 
-            if (result[0] > 0) {
-                // console.log(result[0], '개의 행이 업데이트 됨!')
-                return { result: true, message: '수정 완료' }
-            } else {
-                return { result: false, message: '수정 실패' }
+        const result = await db.Board.update(
+            {
+                board_title: boardDto.title,
+                board_content: boardDto.content,
+            },
+            {
+                where: {
+                    board_id: boardDto.boardId,
+                },
             }
-        } catch (err) {
-            throw err
+        )
+
+        if (result[0] > 0) {
+            // console.log(result[0], '개의 행이 업데이트 됨!')
+            return { result: true, message: '수정 완료' }
+        } else {
+            return { result: false, message: '수정 실패' }
         }
     }
 
     async getBoardById(boardDto) {
         // 게시글 아이디와 일치하는 게시글 데이터 반환
-        try {
-            const foundBoard = await db.Board.findOne({
-                where: { board_id: boardDto.boardId },
-            })
 
-            if (foundBoard instanceof db.Board) {
-                return {
-                    result: true,
-                    message: '게시글 조회 성공',
-                    data: foundBoard,
-                }
-            } else if (foundBoard === null) {
-                return {
-                    result: false,
-                    message: '게시글 미존재',
-                    data: null,
-                }
-            } else {
-                return {
-                    result: false,
-                    message: '게시글 조회 오류',
-                    data: null,
-                }
+        const foundBoard = await db.Board.findOne({
+            where: { board_id: boardDto.boardId },
+        })
+
+        if (foundBoard instanceof db.Board) {
+            return {
+                result: true,
+                message: '게시글 조회 성공',
+                data: foundBoard,
             }
-        } catch (err) {
-            throw err
+        } else if (foundBoard === null) {
+            return {
+                result: false,
+                message: '게시글 미존재',
+                data: null,
+            }
+        } else {
+            return {
+                result: false,
+                message: '게시글 조회 오류',
+                data: null,
+            }
         }
     }
 
     async deleteBoardById(boardDto) {
         // 게시글 아이디 boardDto와 일치하는 게시글 데이터 삭제
-        try {
-            const board = await db.Board.findOne({
-                where: { board_id: boardDto.boardId },
-            })
 
-            if (board.board_user_id !== boardDto.userId) {
-                return { result: false, message: '삭제 권한 없음' }
-            }
+        const board = await db.Board.findOne({
+            where: { board_id: boardDto.boardId },
+        })
 
-            const deletedRowNum = await db.Board.destroy({
-                where: { board_id: boardDto.boardId },
-            }) // 반환값 : 삭제된 열의 갯수
+        if (board.board_user_id !== boardDto.userId) {
+            return { result: false, message: '삭제 권한 없음' }
+        }
 
-            if (deletedRowNum > 0) {
-                return { result: true, message: '삭제 성공' }
-            } else {
-                return { result: false, message: '삭제 실패' }
-            }
-        } catch (err) {
-            throw err
+        const deletedRowNum = await db.Board.destroy({
+            where: { board_id: boardDto.boardId },
+        }) // 반환값 : 삭제된 열의 갯수
+
+        if (deletedRowNum > 0) {
+            return { result: true, message: '삭제 성공' }
+        } else {
+            return { result: false, message: '삭제 실패' }
         }
     }
 }
