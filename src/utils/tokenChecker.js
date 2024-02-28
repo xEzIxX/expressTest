85e21e12d2dcf82af6e8eb4f78f48098b2d785ca
xEzIxX <by1094@gachon.ac.kr>
2024-02-28 23:10:04 +0900

    refactor : 토큰의 유효성을 판단하는 미들웨어 utils/tokenChecker.js 수정 1. 새로고침 시 header에 존재하였던 토큰이 사라지는 문제로, cookie에 jwt 토큰을 넣어두는 방식을 사용하기로 함 2. 토큰 에러 발생 시 에러 메세지를 응답하는 것이 아닌, 에러 메세지를 req.tokenMessage에 넣어두도록 수정

--- src/utils/tokenChecker.js
+++ src/utils/tokenChecker.js
@@ -4,22 +4,32 @@ import jwt from 'jsonwebtoken'
 export function tokenChecker() {
     return async (req, res, next) => {
         try {
-            const token = req.headers.authorization.split(' ')[1]
+            console.log('req.headers.cookie : ', req.headers.cookie)
+
+            const token = req.cookies.token
             const decoded = jwt.verify(token, process.env.SECRET_KEY)
 
             req.userId = decoded.userId
+
+            console.log('토큰 체크 미들웨어 userId : ', req.userId)
             next()
         } catch (err) {
+            let tokenErr
+            console.log('토큰 에러 발생 : ', err)
             if (err.name === 'TokenExpiredError') {
-                return res.status(401).send({ message: '만료된 토큰' })
+                tokenErr = { message: '만료된 토큰' }
             } else if (err.name === 'JsonWebTokenError') {
-                return res.status(401).send({ message: '유효하지 않은 토큰' })
+                tokenErr = { message: '유효하지 않은 토큰' }
             } else if (err.name === 'TypeError') {
-                return res.status(401).send({ message: '잘못된 형식의 토큰' })
-                //TypeError: Cannot read properties of undefined (reading 'split')
+                tokenErr = { message: '잘못된 형식의 토큰' }
+                // TypeError: Cannot read properties of undefined (reading 'split')
             } else {
                 return next(err)
             }
+
+            req.tokenMessage = tokenErr
+
+            next()
         }
     }
 }
