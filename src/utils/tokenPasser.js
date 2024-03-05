453060f1ecfee9adcd35270415f3316ebd9925d9
xEzIxX <by1094@gachon.ac.kr>
2024-03-05 17:29:58 +0900

    chore : tokenChecker -> tokenPasser로 파일 이름 및 함수 이름 수정 1. 토큰이 유효하다면 req.userId에 userId를 담아 보내고,    유효하지 않으면 req.userId는 undefined, req에 에러 메세지를 보내는 함수의 이름 수정 2. 유효하지 않은 경우 사용되는 변수 이름 수정    (tokenErr->message, req.tokenMessage->req.tokenErrMsg)

--- src/utils/tokenPasser.js
+++ src/utils/tokenPasser.js
@@ -1,7 +1,7 @@
 import '../config/env.js'
 import jwt from 'jsonwebtoken'
 
-export function tokenChecker() {
+export function tokenPasser() {
     return async (req, res, next) => {
         try {
             console.log('req.headers.cookie : ', req.headers.cookie)
@@ -11,23 +11,28 @@ export function tokenChecker() {
 
             req.userId = decoded.userId
 
-            console.log('토큰 체크 미들웨어 userId : ', req.userId)
+            console.log('토큰 전달 미들웨어 userId만 넘겨줌 : ', req.userId)
             next()
+
         } catch (err) {
-            let tokenErr
             console.log('토큰 에러 발생 : ', err)
+            let message
+
             if (err.name === 'TokenExpiredError') {
-                tokenErr = { message: '만료된 토큰' }
+                message = '만료된 토큰'
             } else if (err.name === 'JsonWebTokenError') {
-                tokenErr = { message: '유효하지 않은 토큰' }
+                message = '유효하지 않은 토큰'
             } else if (err.name === 'TypeError') {
-                tokenErr = { message: '잘못된 형식의 토큰' }
+                message = '잘못된 형식의 토큰'
+                // token이 undefined일 때
                 // TypeError: Cannot read properties of undefined (reading 'split')
             } else {
-                return next(err)
+                return next(err) // 에러 핸들러로 이동
             }
 
-            req.tokenMessage = tokenErr
+            req.tokenErrMsg = message
+            // req.userId 는 undefined로 넘겨준다
+            // else를 제외한 경우에는 err를 발생시키지 않고, 그냥 에러 메세지만 req에 넘겨줌
 
             next()
         }
