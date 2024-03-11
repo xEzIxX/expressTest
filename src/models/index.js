98c023eed29ea38f51f5d3bc580c9165262dd5f7
xEzIxX <by1094@gachon.ac.kr>
2024-03-12 01:32:54 +0900

    refactor : models/index.js 수정 1. 별칭 삭제, 삭제하였던 외래키 속성 추가

--- src/models/index.js
+++ src/models/index.js
@@ -33,59 +33,60 @@ db.User_follow = User_follow(sequelize, DataTypes)
 db.User.hasMany(db.Comment, {
     foreignKey: 'comment_user_id',
 })
-db.Comment.belongsTo(db.User)
+db.Comment.belongsTo(db.User, {
+    foreignKey: 'comment_user_id',
+})
 
 db.Board.hasMany(db.Comment, {
     foreignKey: 'comment_board_id',
 })
-db.Comment.belongsTo(db.Board)
+db.Comment.belongsTo(db.Board, {
+    foreignKey: 'comment_board_id',
+})
 
 db.User.hasMany(db.Board, {
     foreignKey: 'board_user_id',
 })
-db.Board.belongsTo(db.User)
-
-db.User.hasMany(db.Board_like, {
-    foreignKey: 'board_like_user',
+db.Board.belongsTo(db.User, {
+    foreignKey: 'board_user_id',
 })
-db.Board_like.belongsTo(db.User)
 
 // 게시글 좋아요 Board_like 테이블의 관계
-db.Board.hasMany(db.Board_like, {
-    // 게시글 테이블은 게시글 좋아요 테이블과 1:N 관계
-    as: 'board_like',
-    foreignKey: 'board_like_board_id',
-})
-db.Board_like.belongsTo(db.Board)
-
 db.User.hasMany(db.Board_like, {
-    // 유저 테이블은 게시글 좋아요 테이블과 1:N 관계
-    as: 'user_like_board',
     foreignKey: 'board_like_user_id',
 })
-db.Board_like.belongsTo(db.User)
+db.Board_like.belongsTo(db.User, {
+    foreignKey: 'board_like_user_id',
+})
+
+db.Board.hasMany(db.Board_like, {
+    foreignKey: 'board_like_board_id',
+})
+db.Board_like.belongsTo(db.Board, {
+    foreignKey: 'board_like_board_id',
+})
 
 // 댓글 좋아요 Comment_like 테이블의 관계
 db.Comment.hasMany(db.Comment_like, {
-    // 댓글 테이블은 댓글 좋아요 테이블과 1:N 관계
-    as: 'comment_like',
     foreignKey: 'comment_like_comment_id',
 })
-db.Comment_like.belongsTo(db.Comment)
+db.Comment_like.belongsTo(db.Comment, {
+    foreignKey: 'comment_like_comment_id',
+})
 
 db.User.hasMany(db.Comment_like, {
-    // 유저 테이블은 댓글 좋아요 테이블과 1:N 관계
-    as: 'user_like_comment',
     foreignKey: 'comment_like_user_id',
 })
-db.Comment_like.belongsTo(db.User)
+db.Comment_like.belongsTo(db.User, {
+    foreignKey: 'comment_like_user_id',
+})
 
 // 유저 팔로우 User_follow 테이블의 관계
 db.User.hasMany(db.User_follow, {
-    // 댓글 테이블은 댓글 좋아요 테이블과 1:N 관계
-    as: 'user_follower',
     foreignKey: 'user_follow_follower_id',
 })
-db.User_follow.belongsTo(db.User)
+db.User_follow.belongsTo(db.User, {
+    foreignKey: 'user_follow_follower_id',
+})
 
 export { db }
